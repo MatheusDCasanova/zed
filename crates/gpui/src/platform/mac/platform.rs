bb9b8d4136b56426bff940f4614026dd33fc3a87
Matheus Casanova <matheuscasanova02@gmail.com>
2024-06-22 16:05:44 -0300

    Removes message sending. Lets setCanCreateDirectories bet optional.

--- crates/gpui/src/platform/mac/platform.rs
+++ crates/gpui/src/platform/mac/platform.rs
@@ -611,10 +611,8 @@ impl Platform for MacPlatform {
                     panel.setCanChooseDirectories_(options.directories.to_objc());
                     panel.setCanChooseFiles_(options.files.to_objc());
                     panel.setAllowsMultipleSelection_(options.multiple.to_objc());
-                    panel.setCanCreateDirectories(true.to_objc());
+                    panel.setCanCreateDirectories(options.allow_create_directories.to_objc());
                     panel.setResolvesAliases_(false.to_objc());
-                    // Set the canCreateDirectories property using the Objective-C runtime
-                    let _: () = msg_send![panel, setCanCreateDirectories: options.allow_create_directories.to_objc()];
                     let done_tx = Cell::new(Some(done_tx));
                     let block = ConcreteBlock::new(move |response: NSModalResponse| {
                         let result = if response == NSModalResponse::NSModalResponseOk {
